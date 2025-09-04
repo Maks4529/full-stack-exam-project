612bcac55202ae53fe8a038fac9b423e1c789298
Maks4529 <max555gos@gmail.com>
2025-09-04 19:15:52 +0300

    Add section 2

--- client/src/pages/HowItWorksPage/HowItWorksPage.jsx
+++ client/src/pages/HowItWorksPage/HowItWorksPage.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import Header from '../../components/HowItWorks/Header/Header';
+import ItemCard from '../../components/HowItWorks/Card/ItemCard/ItemCard';
+import CONSTANTS from '../../constants';
 import styles from './HowItWorks.module.sass';
 
 class HowItWorksPage extends Component {
@@ -8,7 +10,7 @@ class HowItWorksPage extends Component {
         <>
       <Header />
       <main className={styles.howItWorksPage}>
-        <div className={styles.container1}>
+        <section className={styles.container1}>
         <div className={styles.titleContainer}>
           <div className={styles.blueMessage}>World's #1 Naming Platform</div>
           <h1>How Does Atom Work?</h1>
@@ -29,7 +31,19 @@ class HowItWorksPage extends Component {
             allowFullScreen
           ></iframe>
         </div>
-        </div>
+        </section>
+        <section className={styles.container2}>
+          <div className={styles.blueMessage}>Our Services</div>
+          <h2>3 Ways To Use Atom</h2>
+          <p className={styles.description}>
+            Atom offers 3 ways to get you a perfect name for your business.
+          </p>
+          <div className={styles.cardsContainer}>
+            <ItemCard  icon={`${CONSTANTS.STATIC_IMAGES_PATH}lightning.svg`} iconAlt={`lighting`} title={'Launch a Contest'} description={'Work with hundreds of creative experts to get custom name suggestions for your business or brand. All names are auto-checked for URL availability.'} link={'/startContest'} linkBody={'Launch a Contest'}/>
+            <ItemCard  icon={`${CONSTANTS.STATIC_IMAGES_PATH}computer.svg`} iconAlt={`computer`} title={'Explore Names For Sale'} description={'Our branding team has curated thousands of pre-made names that you can purchase instantly. All names include a matching URL and a complimentary Logo Design.'} link={'/namesForSale'} linkBody={'Explore Names For Sale'}/>
+            <ItemCard  icon={`${CONSTANTS.STATIC_IMAGES_PATH}lamp.svg`} iconAlt={`lamp`} title={'Agency-level Managed Contests'} description={'Our Managed contests combine the power of crowdsourcing with the rich experience of our branding consultants. Get a complete agency-level experience at a fraction of Agency costs.'} link={'/managedContests'} linkBody={'Learn More'}/>
+          </div>
+        </section>
       </main>
         </>
     );
