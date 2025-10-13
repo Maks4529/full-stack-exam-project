547e533e0b1c923b61fe0cad046ebde37d33485c
Maks4529 <max555gos@gmail.com>
2025-10-13 18:58:40 +0300

    Fix DialogBox merge leftovers: remove conflict markers and duplicated code

--- client/src/components/Chat/DialogComponents/DialogBox/DialogBox.jsx
+++ client/src/components/Chat/DialogComponents/DialogBox/DialogBox.jsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import styles from './DialogBox.module.sass';
 import CONSTANTS from '../../../../constants';
 
-function DialogBox (props) {
+function DialogBox(props) {
   const {
     chatPreview,
     userId,
@@ -15,10 +15,12 @@ function DialogBox (props) {
     chatMode,
     interlocutor,
   } = props;
-  const { favoriteList, participants, blackList, _id, text, createAt } =
-    chatPreview;
-  const isFavorite = favoriteList[participants.indexOf(userId)];
-  const isBlocked = blackList[participants.indexOf(userId)];
+
+  const { favoriteList = [], participants = [], blackList = [], _id, text, createAt } = chatPreview || {};
+  const participantIndex = participants.indexOf(userId);
+  const isFavorite = Boolean(favoriteList?.[participantIndex]);
+  const isBlocked = Boolean(blackList?.[participantIndex]);
+
   return (
     <div
       className={styles.previewChatBox}
@@ -35,141 +37,70 @@ function DialogBox (props) {
       }
     >
       <img
-<<<<<<< HEAD
-  src={
-    interlocutor?.avatar === 'anon.png'
-      ? CONSTANTS.ANONYM_IMAGE_PATH
-      : `${CONSTANTS.publicURL}${interlocutor?.avatar ?? 'anon.png'}`
-  }
-  alt="user"
-/>
-=======
         src={
-          interlocutor.avatar === 'anon.png'
+          interlocutor?.avatar === 'anon.png'
             ? CONSTANTS.ANONYM_IMAGE_PATH
-            : `${CONSTANTS.publicURL}${interlocutor.avatar}`
+            : `${CONSTANTS.publicURL}${interlocutor?.avatar ?? 'anon.png'}`
         }
-        alt='user'
+        alt="user"
       />
->>>>>>> fullstack2
+
       <div className={styles.infoContainer}>
-        import React from 'react';
-        import classNames from 'classnames';
-        import styles from './DialogBox.module.sass';
-        import CONSTANTS from '../../../../constants';
+        <div className={styles.interlocutorInfo}>
+          <span className={styles.interlocutorName}>{interlocutor?.firstName ?? 'Anonimous'}</span>
+          <span className={styles.interlocutorMessage}>{text}</span>
+        </div>
+        <div className={styles.buttonsContainer}>
+          <span className={styles.time}>{getTimeStr(createAt)}</span>
 
-        function DialogBox(props) {
-          const {
-            chatPreview,
-            userId,
-            getTimeStr,
-            changeFavorite,
-            changeBlackList,
-            catalogOperation,
-            goToExpandedDialog,
-            chatMode,
-            interlocutor,
-          } = props;
+          <i
+            onClick={(event) =>
+              changeFavorite(
+                {
+                  participants,
+                  participantsConversationId: _id,
+                  favoriteFlag: !isFavorite,
+                },
+                event
+              )
+            }
+            className={classNames({
+              'far fa-heart': !isFavorite,
+              'fas fa-heart': isFavorite,
+            })}
+          />
 
-          const { favoriteList = [], participants = [], blackList = [], _id, text, createAt } = chatPreview || {};
-          const participantIndex = participants.indexOf(userId);
-          const isFavorite = Boolean(favoriteList?.[participantIndex]);
-          const isBlocked = Boolean(blackList?.[participantIndex]);
+          <i
+            onClick={(event) =>
+              changeBlackList(
+                {
+                  participants,
+                  participantsConversationId: _id,
+                  blackListFlag: !isBlocked,
+                },
+                event
+              )
+            }
+            className={classNames({
+              'fas fa-user-lock': !isBlocked,
+              'fas fa-unlock': isBlocked,
+            })}
+          />
 
-          return (
-            <div
-              className={styles.previewChatBox}
-              onClick={() =>
-                goToExpandedDialog({
-                  interlocutor,
-                  conversationData: {
-                    participants,
-                    _id,
-                    blackList,
-                    favoriteList,
-                  },
-                })
-              }
-            >
-              <img
-                src={
-                  interlocutor?.avatar === 'anon.png'
-                    ? CONSTANTS.ANONYM_IMAGE_PATH
-                    : `${CONSTANTS.publicURL}${interlocutor?.avatar ?? 'anon.png'}`
-                }
-                alt="user"
-              />
+          <i
+            onClick={(event) => catalogOperation(event, _id)}
+            className={classNames({
+              'far fa-plus-square': chatMode !== CONSTANTS.CATALOG_PREVIEW_CHAT_MODE,
+              'fas fa-minus-circle': chatMode === CONSTANTS.CATALOG_PREVIEW_CHAT_MODE,
+            })}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
 
-              <div className={styles.infoContainer}>
-                import React from 'react';
-                import classNames from 'classnames';
-                import styles from './DialogBox.module.sass';
-                import CONSTANTS from '../../../../constants';
-
-                function DialogBox(props) {
-                  const {
-                    chatPreview,
-                    userId,
-                    getTimeStr,
-                    changeFavorite,
-                    changeBlackList,
-                    catalogOperation,
-                    goToExpandedDialog,
-                    chatMode,
-                    interlocutor,
-                  } = props;
-
-                  const { favoriteList = [], participants = [], blackList = [], _id, text, createAt } = chatPreview || {};
-                  const participantIndex = participants.indexOf(userId);
-                  const isFavorite = Boolean(favoriteList?.[participantIndex]);
-                  const isBlocked = Boolean(blackList?.[participantIndex]);
-
-                  return (
-                    <div
-                      className={styles.previewChatBox}
-                      onClick={() =>
-                        goToExpandedDialog({
-                          interlocutor,
-                          conversationData: {
-                            participants,
-                            _id,
-                            blackList,
-                            favoriteList,
-                          },
-                        })
-                      }
-                    >
-                      <img
-                        src={
-                          interlocutor?.avatar === 'anon.png'
-                            ? CONSTANTS.ANONYM_IMAGE_PATH
-                            : `${CONSTANTS.publicURL}${interlocutor?.avatar ?? 'anon.png'}`
-                        }
-                        alt="user"
-                      />
-
-                      <div className={styles.infoContainer}>
-                        <div className={styles.interlocutorInfo}>
-                          <span className={styles.interlocutorName}>{interlocutor?.firstName ?? 'Anonimous'}</span>
-                          <span className={styles.interlocutorMessage}>{text}</span>
-                        </div>
-                        <div className={styles.buttonsContainer}>
-                          <span className={styles.time}>{getTimeStr(createAt)}</span>
-
-                          <i
-                            onClick={(event) =>
-                              changeFavorite(
-                                {
-                                  participants,
-                                  participantsConversationId: _id,
-                                  favoriteFlag: !isFavorite,
-                                },
-                                event
-                              )
-                            }
-                            className={classNames({
-                              'far fa-heart': !isFavorite,
-                              'fas fa-heart': isFavorite,
+export default DialogBox;
                             })}
                           />
 
