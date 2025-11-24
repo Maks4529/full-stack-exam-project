9b57d16e4dbfacea578f3a090ae7688b4fd317cf
Maks4529 <max555gos@gmail.com>
2025-11-24 21:54:03 +0200

    Fix: script to start project

--- start-dev.sh
+++ start-dev.sh
@@ -8,17 +8,19 @@
 started_at=$(date +"%s")
 
 echo "-----> Provisioning containers"
-docker compose --file docker-compose-dev.yaml up
+docker compose --file docker-compose-dev.yaml up -d
 echo ""
+
+SERVER_CONTAINER=$(docker compose -f docker-compose-dev.yaml ps -q server-dev)
 
 # Run Sequalize's migrations.
 echo "-----> Running application migrations"
-docker exec -it exam-project-server-dev-1 sequelize db:migrate
+docker exec -it $SERVER_CONTAINER sequelize db:migrate
 echo ""
 
 # Run Sequalize's seeds.
 echo "-----> Running application seeds"
-docker exec -it exam-project-server-dev-1 sequelize db:seed:all
+docker exec -it $SERVER_CONTAINER sequelize db:seed:all
 echo "<----- Seeds created"
 
 ended_at=$(date +"%s")
